5b8f7d1d71f157bf321b00e6ad17b483379a0354
banunn <brock@brocknunn.com>
2015-08-11 21:29:04 -0500

    Added Hero Slider + Various Home page nodes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+// Navigation
+
 $('.nav-toggle').click(function(){
   $('.nav-top ul').toggleClass('open');
+});
+
+
+// Slick
+
+$(document).ready(function(){
+  $('.hero-slider').slick({
+    arrows: true,
+    centerMode: true,
+    centerPadding: '35px',
+    slidesToShow: 2,
+    initialSlide: 2,
+    cssEase: 'linear',
+    autoplaySpeed: 5500,
+    responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: '25px',
+      slidesToShow: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      arrows: false,
+      centerMode: false,
+      centerPadding: '30px',
+      slidesToShow: 1
+    }
+  }
+]
+  });
 });
